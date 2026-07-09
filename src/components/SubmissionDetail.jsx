eaa83abdc78050b5c24831eb469b6a15e0ef9589
Esmael <esmael6360@gmail.com>
2026-07-09 16:10:23 +0300

    lk

--- src/components/SubmissionDetail.jsx
+++ src/components/SubmissionDetail.jsx
@@ -2,7 +2,7 @@ import StatusBadge from './UI';
 import { formatDate } from '../utils/storage';
 import './SubmissionTable.css';
 import './ImageUpload.css';
-// comment
+// commen
 export default function SubmissionDetail({ item, type, history = [], departmentName, onClose, actions }) {
   if (!item) return null;
 
