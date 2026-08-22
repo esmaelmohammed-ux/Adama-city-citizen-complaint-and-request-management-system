6062e71f28a7be64e3e48d0a2002243f3d91a461
Esmael <esmael6360@gmail.com>
2026-08-22 15:09:48 +0300

    lk

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -7,7 +7,7 @@ import LanguageSwitcher from './LanguageSwitcher';
 import './BrandLogo.css';
 import './LanguageSwitcher.css';
 import './Layout.css';
-// comment
+// comment added by me
 const navByRole = {
   [ROLES.CITIZEN]: [
     { to: '/citizen', labelKey: 'sidebar.dashboard', end: true },
