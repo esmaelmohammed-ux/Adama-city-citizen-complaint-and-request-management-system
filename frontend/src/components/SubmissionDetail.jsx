f082c14ace00a974856527c27f596fa55756d9f3
Esmael <esmael6360@gmail.com>
2026-08-28 15:37:22 +0300

    add brand logo

--- frontend/src/components/SubmissionDetail.jsx
+++ frontend/src/components/SubmissionDetail.jsx
@@ -4,7 +4,7 @@ import { formatDate } from '../utils/storage';
 import { formatComplaintLocation } from '../utils/location';
 import './SubmissionTable.css';
 import './ImageUpload.css';
-// comment added by me
+// comment added by m
 export default function SubmissionDetail({
   item,
   history = [],
