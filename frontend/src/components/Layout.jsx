8ea29f7df48b7d2b3ebcc08df0d2dfbc417494c1
Esmael <esmael6360@gmail.com>
2026-08-28 16:14:00 +0300

    add brand logo

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -7,7 +7,7 @@ import LanguageSwitcher from './LanguageSwitcher';
 import './BrandLogo.css';
 import './LanguageSwitcher.css';
 import './Layout.css';
-// comment added by me
+// comment added by m
 const navByRole = {
   [ROLES.CITIZEN]: [
     { to: '/citizen', labelKey: 'sidebar.dashboard', end: true },
