ed31dbfc62283c135181ebe170049611b9dc09e3
Esmael <esmael6360@gmail.com>
2026-08-31 18:07:50 +0300

    lk

--- frontend/src/components/SubmissionTable.jsx
+++ frontend/src/components/SubmissionTable.jsx
@@ -3,7 +3,7 @@ import { STATUSES } from '../constants';
 import { useLanguage } from '../context/LanguageContext';
 import { formatDate } from '../utils/storage';
 import './SubmissionTable.css';
-// comment added by m
+// comment added by me
 export default function SubmissionTable({
   items,
   showCitizen = false,
